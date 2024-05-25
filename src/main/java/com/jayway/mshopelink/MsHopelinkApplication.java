441309ca8355e7d0837e20d6f9f91f2b94c298e0
oliveraluiss11 <loliverv11@gmail.com>
2024-05-24 20:03:35 -0500

    MAIN
    Inicio del proyecto

---
+++ src/main/java/com/jayway/mshopelink/MsHopelinkApplication.java
@@ -0,0 +1,13 @@
+package com.jayway.mshopelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHopelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHopelinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayway/mshopelink/MsHopelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayway.mshopelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHopelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
