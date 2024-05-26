ca598e170d1322698adc0bf6b20c5f24f4ad5794
oliveraluiss11 <loliverv11@gmail.com>
2024-05-25 22:26:39 -0500

    DEVELOP
    Estabilización

--- src/main/java/com/jayway/mshopelink/modules/donation/infrastructure/DonationRepositoryImpl.java
+++ src/main/java/com/jayway/mshopelink/modules/donation/infrastructure/DonationRepositoryImpl.java
@@ -1,16 +1,17 @@
 package com.jayway.mshopelink.modules.donation.infrastructure;
 
-import com.jayway.mshopelink.modules.donation.domain.repository.DonationRepository;
 import com.jayway.mshopelink.modules.donation.domain.aggregateroute.Donation;
+import com.jayway.mshopelink.modules.donation.domain.repository.DonationRepository;
 import com.jayway.mshopelink.modules.donation.infrastructure.persistence.DonationDocument;
 import com.jayway.mshopelink.modules.donation.infrastructure.persistence.DonationMongoRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
-@Repository
+@Component
 @RequiredArgsConstructor
-public class DonationMongoRepositoryImpl implements DonationRepository {
+public class DonationRepositoryImpl implements DonationRepository {
     private final DonationMongoRepository donationMongoRepository;
+
     @Override
     public void save(Donation donation) {
         donationMongoRepository.save(DonationDocument.from(donation));
