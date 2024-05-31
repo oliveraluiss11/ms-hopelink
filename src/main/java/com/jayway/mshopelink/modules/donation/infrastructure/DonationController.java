c62507de60164bb9cd1e7ac4ac7bd335d08aab27
christopher-sanchez_pacifico <christopher.sanchez@inetum.com>
2024-05-31 11:19:22 -0500

    show traces in console and files

--- src/main/java/com/jayway/mshopelink/modules/donation/infrastructure/DonationController.java
+++ src/main/java/com/jayway/mshopelink/modules/donation/infrastructure/DonationController.java
@@ -6,10 +6,7 @@ import com.jayway.mshopelink.modules.donation.domain.dto.RegisterDonationRequest
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.jayway.mshopelink.commons.Constants.REQUEST_ERROR;
 import static com.jayway.mshopelink.commons.Utils.buildErrorResponse;
@@ -21,7 +18,7 @@ public class DonationController  {
     private final RegisterDonation registerDonation;
 
     @GetMapping
-    public ResponseEntity<Void> registerDonation(RegisterDonationRequest request) {
+    public ResponseEntity<Void> registerDonation(@RequestBody RegisterDonationRequest request) {
         registerDonation.register(request);
         return ResponseEntity.ok().build();
     }
